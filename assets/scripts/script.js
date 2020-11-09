b1f486dcf3c162136e111300da7e8367ab792d9f
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-09 11:13:26 -0500

    Added in 10 minute refresh, last page update notice, api calls for metro and weather

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,31 +3,77 @@
 
 console.log ("Inside Script");
 
+// Auto page refresh every 5 minutes
+setTimeout(function(){
+    window.location.reload(1);
+ }, 600000);
+
+
 $ (document).ready(function(){
 
-    const PUBLICMETROKEY = `e13626d03d8e4c03ac07f95541b3091b`;
-    const PUBLICWEATHERKEY = `f9c22785936f5fc5811e20fb8cb7e2fc`
+    const PUBLICMETROKEY = `e13626d03d8e4c03ac07f95541b3091b`; 
+    const PUBLICWEATHERKEY = `f9c22785936f5fc5811e20fb8cb7e2fc`; // removed c from end
     const metroAPI = `https://api.wmata.com/Incidents.svc/json/Incidents?api_key=${PUBLICMETROKEY}`;
     const busAPI = `https://api.wmata.com/Incidents.svc/json/BusIncidents?api_key=${PUBLICMETROKEY}`;
     const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?zip=20500&units=imperial&appid=${PUBLICWEATHERKEY}`;
+    let myTime = new Date();
+    const currentTime = `${myTime.getHours()}:${myTime.getMinutes()}`;
 
-    console.log (metroAPI);
-    console.log (busAPI);
-    console.log (weatherAPI);
+    // console.log (currentTime);
+    $('#lastUpdate').text(currentTime);
+
+    const displayWeather = (weatherDetails) => {
+        let weatherIcon = `<img src='http://openweathermap.org/img/wn/${weatherDetails.weather[0].icon}@2x.png' alt='Weather Icon'>`;
+        console.log (weatherDetails);
+        $('#weatherLat').text(weatherDetails.coord.lat)
+        $('#weatherLong').text(weatherDetails.coord.lon)
+        $('#currentTemp').text(Math.round(weatherDetails.main.temp));
+        $('#feelsLike').text(Math.round(weatherDetails.main.feels_like));
+        $('#currentHumidity').text(weatherDetails.main.humidity);
+        $('#forecastHigh').text(weatherDetails.main.temp_max);
+        $('#currentConditions').text(weatherDetails.weather[0].description);
+        $('#currentWindSpeed').text(weatherDetails.wind.speed);
+        $('#currentWindDirection').text(weatherDetails.wind.deg);
+        $('#weatherIcon').html(weatherIcon);
+
+    }
 
     const busResults = () => {
-        console.log ("Inside Bus Results function")
-        $.ajax(
+        $.get(
             {
                 url: busAPI,
-                method: "GET"
+            })
+            .done ( function (res) {
+                console.log (res);
+            })
+            .fail (function (error) {
+                console.log(error);
+            })
+    }
+
+    const metroResults = () => {
+        $.get(
+            {
+                url: metroAPI,
             })
         .then (function (res) {
             console.log (res);
         })
     }
 
+    const weatherResults = () => {
+        $.get(
+            {
+                url: weatherAPI,
+            })
+        .then (function (res) {
+            displayWeather(res);
+        })
+    }
+    
     busResults();
+    metroResults();
+    weatherResults();
 
 
     // No code beyond here, end of JQuery
